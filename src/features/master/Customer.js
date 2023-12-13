8a72a8cfeffbee055ac7c61225f97b416fd6f1e1
Vignesh <vignesh@whydigit.com>
2023-12-13 18:07:01 +0530

    Table Updation on blw screens

--- src/features/master/Customer.js
+++ src/features/master/Customer.js
@@ -1,5 +1,8 @@
-import * as React from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import React, { useMemo } from "react";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
 import DashBoardComponent from "./DashBoardComponent";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
@@ -14,8 +17,6 @@ import { IoMdClose } from "react-icons/io";
 import { MdGroups } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { FaDatabase } from "react-icons/fa6";
-import AddUsers from "./AddUsers";
-import { CiSettings } from "react-icons/ci";
 import AddCustomer from "./AddCustomer";
 const statsData = [
   {
@@ -53,7 +54,7 @@ const columns = [
   { field: "Status", headerName: "Status", width: 120 },
 ];
 
-const rows = [
+const data = [
   {
     id: 1,
     CustomerName: "John",
@@ -68,6 +69,7 @@ const rows = [
 function Customer() {
   const [open, setOpen] = React.useState(false);
   const [add, setAdd] = React.useState(false);
+  const [data, setData] = React.useState([]);
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -76,7 +78,7 @@ function Customer() {
     setOpen(false);
   };
 
-  const handleAddOpen = () => {
+  const handleAdd = () => {
     setAdd(true);
   };
 
@@ -94,6 +96,103 @@ function Customer() {
     left: 0,
     whiteSpace: "nowrap",
     width: 1,
+  });
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "id",
+        header: "Sr No",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "first",
+        },
+        muiTableBodyCellProps: {
+          align: "first",
+        },
+      },
+      {
+        accessorKey: "name",
+        header: "Name",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "sku",
+        header: "SKU",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "category",
+        header: "Category",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "purchaseprice",
+        header: "Purchase Price",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "salesprice",
+        header: "Sales Price",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "hsn",
+        header: "HSN",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "status",
+        header: "Status",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+    ],
+    []
+  );
+  const table = useMaterialReactTable({
+    data,
+    columns,
   });
 
   return (
@@ -117,52 +216,17 @@ function Customer() {
               >
                 Upload
               </button>
-              <div className="flex justify-start gap-x-2">
-                <div className="w-64">
-                  <div className="relative flex w-full flex-wrap items-stretch">
-                    <input
-                      type="search"
-                      className="relative h-fit m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-white bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                      placeholder="Search"
-                      aria-label="Search"
-                      aria-describedby="button-addon1"
-                    />
-
-                    <button
-                      className="bg-blue h-fit relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
-                      type="button"
-                      id="button-addon1"
-                      data-te-ripple-init
-                      data-te-ripple-color="light"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="h-4 w-4"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-                <button
-                  type="button"
-                  onClick={handleAddOpen}
-                  className="bg-blue inline-block rounded bg-primary h-fit px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                >
-                  Add Customer
-                </button>
-                <CiSettings className="cursor-pointer flex mt-0.5 h-[32px] w-[32px] justify-center rounded-lg bg-white text-center shadow shadow-black/10 dark:shadow-black/40" />
-              </div>
+              <button
+                type="button"
+                onClick={handleAdd}
+                className="bg-blue inline-block rounded bg-primary h-fit px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              >
+                Add Customer
+              </button>
             </div>
           </div>
-          <div style={{ height: 400, width: "100%", marginTop: 30 }}>
-            <DataGrid rows={rows} columns={columns} className="bg-white" />
+          <div className="mt-4">
+            <MaterialReactTable table={table} />
           </div>
           <Dialog
             fullWidth={true}
--- src/features/master/Items.js
+++ src/features/master/Items.js
@@ -1,5 +1,8 @@
-import * as React from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import React, { useMemo } from "react";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
 import DashBoardComponent from "./DashBoardComponent";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
@@ -14,7 +17,6 @@ import { IoMdClose } from "react-icons/io";
 import { FaBox } from "react-icons/fa";
 import { FaRegObjectGroup } from "react-icons/fa6";
 import { LuTimerReset } from "react-icons/lu";
-import { CiSettings } from "react-icons/ci";
 import AddItem from "./AddItems";
 const statsData = [
   {
@@ -42,35 +44,11 @@ const statsData = [
     description: "",
   },
 ];
-const columns = [
-  { field: "id", headerName: "Sr. No", width: 90 },
-  { field: "Name", headerName: "Name", width: 90 },
-  { field: "SKU", headerName: "SKU", width: 90 },
-  { field: "Category", headerName: "Category", width: 100 },
-  { field: "Dimensions", headerName: "Dimensions", width: 10 },
-  { field: "PurchasePrice", headerName: "Purchase Price", width: 120 },
-  { field: "SalesPrice", headerName: "Sales Price", width: 110 },
-  { field: "HSN", headerName: "HSN", width: 100 },
-  { field: "Status", headerName: "Status", width: 90 },
-];
-
-const rows = [
-  {
-    id: 1,
-    Name: "Snow",
-    SKU: "Jon",
-    Category: "texas",
-    Dimensions: "123",
-    PurchasePrice: "1000",
-    SalesPrice: "1500",
-    HSN: "Active",
-    Status: "Active",
-  },
-];
 
 function Items() {
   const [open, setOpen] = React.useState(false);
-  const [addItem, setAddItem] = React.useState(false);
+  const [add, setAdd] = React.useState(false);
+  const [data, setData] = React.useState([]);
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -79,12 +57,12 @@ function Items() {
     setOpen(false);
   };
 
-  const handleAddItem = () => {
-    setAddItem(true);
+  const handleAddOpen = () => {
+    setAdd(true);
   };
 
   const handleBack = () => {
-    setAddItem(false);
+    setAdd(false);
   };
 
   const VisuallyHiddenInput = styled("input")({
@@ -98,10 +76,107 @@ function Items() {
     whiteSpace: "nowrap",
     width: 1,
   });
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "id",
+        header: "Sr No",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "first",
+        },
+        muiTableBodyCellProps: {
+          align: "first",
+        },
+      },
+      {
+        accessorKey: "name",
+        header: "Name",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "sku",
+        header: "SKU",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "category",
+        header: "Category",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "purchaseprice",
+        header: "Purchase Price",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "salesprice",
+        header: "Sales Price",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "hsn",
+        header: "HSN",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+      {
+        accessorKey: "status",
+        header: "Status",
+        size: 50,
+        muiTableHeadCellProps: {
+          align: "center",
+        },
+        muiTableBodyCellProps: {
+          align: "center",
+        },
+      },
+    ],
+    []
+  );
+  const table = useMaterialReactTable({
+    data,
+    columns,
+  });
 
   return (
     <>
-      {addItem ? (
+      {add ? (
         <AddItem addItem={handleBack} />
       ) : (
         <div className="card w-full p-6 bg-base-100 shadow-xl">
@@ -120,52 +195,17 @@ function Items() {
               >
                 Upload
               </button>
-              <div className="flex justify-start gap-x-2">
-                <div className="w-64">
-                  <div className="relative flex w-full flex-wrap items-stretch">
-                    <input
-                      type="search"
-                      className="relative h-fit m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-white bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                      placeholder="Search"
-                      aria-label="Search"
-                      aria-describedby="button-addon1"
-                    />
-
-                    <button
-                      className="bg-blue h-fit relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
-                      type="button"
-                      id="button-addon1"
-                      data-te-ripple-init
-                      data-te-ripple-color="light"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="h-4 w-4"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-                <button
-                  type="button"
-                  onClick={handleAddItem}
-                  className="bg-blue inline-block rounded bg-primary h-fit px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                >
-                  Add Item
-                </button>
-                <CiSettings className="cursor-pointer flex mt-0.5 h-[32px] w-[32px] justify-center rounded-lg bg-white text-center shadow shadow-black/10 dark:shadow-black/40" />
-              </div>
+              <button
+                type="button"
+                onClick={handleAddOpen}
+                className="bg-blue inline-block rounded bg-primary h-fit px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+              >
+                Add Items
+              </button>
             </div>
           </div>
-          <div style={{ height: 400, width: "100%", marginTop: 30 }}>
-            <DataGrid rows={rows} columns={columns} className="bg-white" />
+          <div className="mt-4">
+            <MaterialReactTable table={table} />
           </div>
           <Dialog
             fullWidth={true}
@@ -216,6 +256,7 @@ function Items() {
             </DialogActions>
           </Dialog>
         </div>
+        // </div>
       )}
     </>
   );
