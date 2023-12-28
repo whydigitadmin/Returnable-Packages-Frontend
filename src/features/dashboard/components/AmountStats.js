6e84cc6f3a035017c2a1ada570b43ba7828a985e
Gopal <bgopal@whydigit.com>
2023-12-28 16:30:42 +0530

    minor changes

--- src/features/dashboard/components/AmountStats.js
+++ src/features/dashboard/components/AmountStats.js
@@ -4,18 +4,18 @@ function AmountStats({}){
     return(
         <div className="stats bg-base-100 shadow">
             <div className="stat">
-                <div className="stat-title">Amount to be Collected</div>
-                <div className="stat-value">$25,600</div>
+                <div className="stat-title">Total Purchase Order</div>
+                <div className="stat-value">1,600</div>
                 <div className="stat-actions">
-                    <button className="btn btn-xs">View Users</button> 
+                    <button className="btn btn-xs">View Orders</button> 
                 </div>
             </div>
             
             <div className="stat">
-                <div className="stat-title">Cash in hand</div>
-                <div className="stat-value">$5,600</div>
+                <div className="stat-title">Pending Order</div>
+                <div className="stat-value">1,450</div>
                 <div className="stat-actions">
-                    <button className="btn btn-xs">View Members</button> 
+                    <button className="btn btn-xs">View Orders</button> 
                 </div>
             </div>
         </div>
--- src/features/dashboard/components/PageStats.js
+++ src/features/dashboard/components/PageStats.js
@@ -1,5 +1,5 @@
-import HeartIcon  from '@heroicons/react/24/outline/HeartIcon'
-import BoltIcon  from '@heroicons/react/24/outline/BoltIcon'
+import BoltIcon from '@heroicons/react/24/outline/BoltIcon'
+import HeartIcon from '@heroicons/react/24/outline/HeartIcon'
 
 
 function PageStats({}){
@@ -10,8 +10,8 @@ function PageStats({}){
     <div className="stat-figure invisible md:visible">
         <HeartIcon className='w-8 h-8'/>
     </div>
-    <div className="stat-title">Total Likes</div>
-    <div className="stat-value">25.6K</div>
+    <div className="stat-title">Latest Order</div>
+    <div className="stat-value">567</div>
     <div className="stat-desc">21% more than last month</div>
   </div>
   
@@ -19,8 +19,8 @@ function PageStats({}){
     <div className="stat-figure invisible md:visible">
         <BoltIcon className='w-8 h-8'/>
     </div>
-    <div className="stat-title">Page Views</div>
-    <div className="stat-value">2.6M</div>
+    <div className="stat-title">Last Receive Order</div>
+    <div className="stat-value">234</div>
     <div className="stat-desc">14% more than last month</div>
   </div>
 </div>
