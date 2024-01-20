c8ab45bb1ca28713e082628ebb66b772152b1418
Karthikeyan <karthikeyan@whydigit.com>
2024-01-20 14:40:53 +0530

    emitter inward

--- src/containers/LeftSidebar.js
+++ src/containers/LeftSidebar.js
@@ -1,57 +1,80 @@
-import routes from '../routes/sidebar'
-import { NavLink,  Routes, Link , useLocation} from 'react-router-dom'
-import SidebarSubmenu from './SidebarSubmenu';
-import XMarkIcon  from '@heroicons/react/24/outline/XMarkIcon'
-import { useDispatch } from 'react-redux';
+import XMarkIcon from "@heroicons/react/24/outline/XMarkIcon";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import routes from "../routes/sidebar";
+import SidebarSubmenu from "./SidebarSubmenu";
 
-function LeftSidebar(){
-    const location = useLocation();
+function LeftSidebar() {
+  const location = useLocation();
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
 
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
 
-    const close = (e) => {
-        document.getElementById('left-sidebar-drawer').click()
-    }
+  const close = () => {
+    setSidebarOpen(false);
+  };
 
-    return(
-        <div className="drawer-side ">
-            <label htmlFor="left-sidebar-drawer" className="drawer-overlay"></label> 
-            <ul className="menu  pt-2 w-64 bg-base-100 text-base-content">
-            <button className="btn btn-ghost bg-base-300  btn-circle z-50 top-0 right-0 mt-4 mr-2 absolute lg:hidden" onClick={() => close()}>
-            <XMarkIcon className="h-5 inline-block w-5"/>
-            </button>
+  return (
+    <div className={`drawer-side ${isSidebarOpen ? "open" : ""}`}>
+      <label
+        htmlFor="left-sidebar-drawer"
+        className="drawer-overlay"
+        onClick={close}
+      ></label>
+      <ul className="menu pt-2 w-64 bg-base-100 text-base-content">
+        <button
+          className="btn btn-ghost bg-base-300 btn-circle z-50 top-0 right-0 mt-4 mr-2 absolute lg:hidden"
+          onClick={toggleSidebar}
+        >
+          <XMarkIcon className="h-5 inline-block w-5" />
+        </button>
 
-                <li className="mb-2 font-semibold text-xl">
-                    
-                    <Link to={'/app/welcome'}><img className="mask mask-squircle w-10" src="/logo192.png" alt="DashWind Logo"/>RPSP</Link> </li>
-                {
-                    routes.map((route, k) => {
-                        return(
-                            <li className="" key={k}>
-                                {
-                                    route.submenu ? 
-                                        <SidebarSubmenu {...route}/> : 
-                                    (<NavLink
-                                        end
-                                        to={route.path}
-                                        className={({isActive}) => `${isActive ? 'font-semibold  bg-base-200 ' : 'font-normal'}`} >
-                                           {route.icon} {route.name}
-                                            {
-                                                location.pathname === route.path ? (<span className="absolute inset-y-0 left-0 w-1 rounded-tr-md rounded-br-md bg-primary "
-                                                aria-hidden="true"></span>) : null
-                                            }
-                                    </NavLink>)
-                                }
-                                
-                            </li>
-                        )
-                    })
-                }
-
-            </ul>
-        </div>
-    )
+        <li className="mb-2 font-semibold text-xl">
+          <Link to={"/app/welcome"}>
+            <img
+              className="mask mask-squircle w-10"
+              src="/logo192.png"
+              alt="DashWind Logo"
+            />
+            RPSP
+          </Link>
+        </li>
+        {routes.map((route, k) => {
+          return (
+            <li className="" key={k}>
+              {route.submenu ? (
+                <SidebarSubmenu {...route} />
+              ) : (
+                <NavLink
+                  end
+                  to={route.path}
+                  className={({ isActive }) =>
+                    `${
+                      isActive ? "font-semibold  bg-base-200 " : "font-normal"
+                    }`
+                  }
+                  onClick={close} // Close sidebar on NavLink click
+                >
+                  {route.icon} {route.name}
+                  {location.pathname === route.path ? (
+                    <span
+                      className="absolute inset-y-0 left-0 w-1 rounded-tr-md rounded-br-md bg-primary "
+                      aria-hidden="true"
+                    ></span>
+                  ) : null}
+                </NavLink>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default LeftSidebar
+export default LeftSidebar;
---
+++ src/features/emitter/EmitterInwardDetails.js
@@ -0,0 +1,311 @@
+import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+import DialogContentText from "@mui/material/DialogContentText";
+import React, { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import TitleCard from "../../components/Cards/TitleCard";
+
+const BILLS = [
+  {
+    rmno: "1072",
+    rmdate: "16-02-2024",
+    ddate: "18-02-2024",
+    issueno: "1001",
+    issuedate: "18-02-2024",
+    kitno: "KIT1702",
+    fname: "GB PUNE TO TATA PUNE",
+    rqty: "10",
+    iqty: "10",
+    nqty: "0",
+    returnqty: "0",
+    tat: "48",
+    pname: "PISTON/PS01",
+    status: "Issued",
+  },
+  {
+    rmno: "1072",
+    rmdate: "16-02-2024",
+    ddate: "18-02-2024",
+    issueno: "1002",
+    issuedate: "18-02-2024",
+    kitno: "KIT1702",
+    fname: "GB PUNE TO TATA PUNE",
+    rqty: "10",
+    iqty: "10",
+    nqty: "0",
+    returnqty: "0",
+    tat: "48",
+    pname: "PISTON/PS01",
+    status: "Issued",
+  },
+  {
+    rmno: "1072",
+    rmdate: "16-02-2024",
+    ddate: "18-02-2024",
+    issueno: "1001",
+    issuedate: "18-02-2024",
+    kitno: "KIT1702",
+    fname: "GB PUNE TO TATA PUNE",
+    rqty: "10",
+    iqty: "10",
+    nqty: "0",
+    returnqty: "0",
+    tat: "48",
+    pname: "PISTON/PS01",
+    status: "Issued",
+  },
+
+  // {
+  //   invoiceNo: "#4523",
+  //   amount: "34,989",
+  //   description: "Product usages",
+  //   status: "Pending",
+  //   generatedOn: moment(new Date())
+  //     .add(-30 * 2, "days")
+  //     .format("DD MMM YYYY"),
+  //   paidOn: "-",
+  // },
+
+  // {
+  //   invoiceNo: "#4453",
+  //   amount: "39,989",
+  //   description: "Product usages",
+  //   status: "Paid",
+  //   generatedOn: moment(new Date())
+  //     .add(-30 * 3, "days")
+  //     .format("DD MMM YYYY"),
+  //   paidOn: moment(new Date())
+  //     .add(-24 * 2, "days")
+  //     .format("DD MMM YYYY"),
+  // },
+];
+
+function EmitterInwardDetails() {
+  const [bills, setBills] = useState(BILLS);
+  const [isPendingPopupOpen, setPendingPopupOpen] = useState(false);
+  const [isPendingPopupOpenIssued, setPendingPopupOpenIssued] = useState(false);
+  const [selectedPendingBill, setSelectedPendingBill] = useState(null);
+
+  const [name, setName] = React.useState("");
+
+  const updateFormValue = (value) => {
+    // Handle the form value update
+    setName(value);
+  };
+
+  const handlePendingStatusClick = (bill) => {
+    setPendingPopupOpen(true);
+    setSelectedPendingBill(bill);
+  };
+
+  const handlePendingStatusClickIssued = (bill) => {
+    setPendingPopupOpenIssued(true);
+    setSelectedPendingBill(bill);
+  };
+
+  const closePendingPopup = () => {
+    setPendingPopupOpen(false);
+    setSelectedPendingBill(null);
+  };
+
+  const closePendingPopupIssued = () => {
+    setPendingPopupOpenIssued(false);
+    setSelectedPendingBill(null);
+  };
+
+  const getPaymentStatus = (status, bill) => {
+    return (
+      <div
+        className="badge bg-success text-white cursor-pointer"
+        onClick={() => handlePendingStatusClickIssued(bill)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          className="h-5 w-5" // Adjust the size as needed
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <TitleCard title="Inward Manifest" topMargin="mt-2">
+        {/* Invoice list in table format loaded constant */}
+        <div className="overflow-x-auto w-full ">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th>RM No.</th>
+                <th>RM Date</th>
+                <th>Demand Date</th>
+                <th>Issue Manifest No</th>
+                <th>Issue Manifest Date</th>
+                <th>Kit No</th>
+                <th>Flow Name</th>
+                <th>Req Qty</th>
+                <th>Issued Qty</th>
+                <th>Net Rec Qty</th>
+                <th>Return</th>
+                <th>TAT (Hrs)</th>
+                <th>Part Name/No</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bills.map((l, k) => {
+                return (
+                  <tr key={k}>
+                    <td>{l.rmno}</td>
+                    <td>{l.rmdate}</td>
+                    <td>{l.ddate}</td>
+                    <td>{l.issueno}</td>
+                    <td>{l.issuedate}</td>
+                    <td>{l.kitno}</td>
+                    <td>{l.fname}</td>
+                    <td>{l.rqty}</td>
+                    <td>{l.iqty}</td>
+                    <td>{l.nqty}</td>
+                    <td>{l.returnqty}</td>
+                    <td>{l.tat}</td>
+                    <td>{l.pname}</td>
+                    <td>{getPaymentStatus(l.status)}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </TitleCard>
+      {/* <Dialog open={isPendingPopupOpen} onClose={closePendingPopup}>
+        <DialogTitle>Issue Manifest</DialogTitle>
+        <DialogContent>
+          <p>NO: 102</p>
+          <p>Date: 19/01/2024</p>
+        </DialogContent>
+        <Button onClick={closePendingPopup}>Close Popup</Button>
+      </Dialog> */}
+      {/* <Dialog
+        fullWidth={true}
+        maxWidth={"sm"}
+        open={isPendingPopupOpen}
+        onClose={closePendingPopup}
+      >
+        <div className="d-flex justify-content-between">
+          <DialogTitle>Issue Manifest</DialogTitle>
+          <IoMdClose
+            className="cursor-pointer w-8 h-8 mt-3 me-3"
+            onClick={closePendingPopup}
+          />
+        </div>
+        <DialogContent>
+          <DialogContentText className="d-flex flex-column">
+            <div className="col-lg-4 text-dark"> No: 1704</div>
+            <div className="col-lg-4 text-dark">Date : 19/01/2022</div>
+            <div className="col-lg-6  text-dark mt-3">
+              Part Name/No : PISTON/PS01
+            </div>
+            <div className="col-lg-6 text-dark ">Demand Qty : 10</div>
+            <div className="col-lg-6 mt-3 font-bold text-xl">Kit No : 1072</div>
+            <div className="your-form-container d-flex flex-wrap">
+              <div className="col-lg-3 text-dark mt-3">Available Qty: 15</div>
+              <div className="col-lg-6 text-dark d-flex flex-row">
+                <label className="label mt-2">
+                  <span className="label-text label-font-size text-base-content d-flex flex-row">
+                    Issued Qty :
+                    <FaStarOfLife className="must" />
+                  </span>
+                </label>
+                <div className="mt-2">
+                  <ToolTip
+                    placeholder="Enter"
+                    content=""
+                    updateFormValue={updateFormValue}
+                    value={name} // Pass the value to ToolTip component if needed
+                  />
+                </div>
+              </div>
+              <div className="col-lg-3 text-dark mt-3 text-end">
+                balance Qty: 5
+              </div>
+            </div>
+          </DialogContentText>
+        </DialogContent>
+        <div className="d-flex justify-content-center">
+          <DialogActions className="mb-2 me-2">
+        
+            <Button
+              component="label"
+              variant="contained"
+              onClick={closePendingPopup}
+            >
+              Issued
+            </Button>
+          </DialogActions>
+        </div>
+        <DialogContentText>
+          <center className="text-dark mb-2">
+            Issued by AIPACKS - Karthi-19/01/2024-10:00AM
+          </center>
+        </DialogContentText>
+      </Dialog> */}
+
+      <Dialog
+        fullWidth={true}
+        maxWidth={"sm"}
+        open={isPendingPopupOpenIssued}
+        onClose={closePendingPopupIssued}
+      >
+        <div className="d-flex justify-content-between">
+          <DialogTitle>Issue Manifest</DialogTitle>
+          <IoMdClose
+            className="cursor-pointer w-8 h-8 mt-3 me-3"
+            onClick={closePendingPopupIssued}
+          />
+        </div>
+        <DialogContent>
+          {/* <DialogContentText className="d-flex flex-column">
+            <div className="col-lg-4 text-dark"> No: 1704</div>
+            <div className="col-lg-4 text-dark">Date : 19/01/2022</div>
+            <div className="col-lg-6  text-dark mt-3">
+              Part Name/No : PISTON/PS01
+            </div>
+            <div className="col-lg-6 text-dark ">Demand Qty : 10</div>
+            <div className="col-lg-6 mt-3 font-bold text-xl">Kit No : 1072</div>
+            <div className="your-form-container d-flex flex-wrap">
+              <div className="col-lg-4 text-dark mt-3">Available Qty: 15</div>
+              <div className="col-lg-4 text-dark mt-3">Issued Qty: 10</div>
+              <div className="col-lg-4 text-dark mt-3">balance Qty: 5</div>
+            </div>
+          </DialogContentText> */}
+        </DialogContent>
+        <div className="d-flex justify-content-center">
+          {/* <DialogActions className="mb-2 me-2">
+            <Button
+              component="label"
+              variant="contained"
+              onClick={closePendingPopupIssued}
+            >
+              Download
+            </Button>
+          </DialogActions> */}
+        </div>
+        <DialogContentText>
+          <center className="text-dark mb-2">
+            Issued by AIPACKS - Karthi-19/01/2024-10:00AM
+          </center>
+        </DialogContentText>
+      </Dialog>
+    </>
+  );
+}
+
+export default EmitterInwardDetails;
