d3769b4f3207b5170707f7a927c807e45113f2ee
Mani-ofl-account <kmanikandan@whydigit.com>
2024-04-06 13:39:19 +0530

    packages installation and assettagging ui changes

--- src/features/asstTagging/AsstTagging.js
+++ src/features/asstTagging/AsstTagging.js
@@ -543,16 +543,19 @@ export const AsstTagging = () => {
         >
           Cancel
         </button>
-      </div>
-      <div className="d-flex flex-row mt-4">
+
         <button
           type="button"
+          style={{ marginLeft: "50px" }}
           onClick={handleGenerateTagcode}
           className="bg-blue me-5 inline-block rounded bg-primary h-fit px-6 pb-2 pt-2.5 text-sm font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
         >
           Generate Tagcode
         </button>
       </div>
+      {/* <div className="d-flex flex-row mt-4">
+
+      </div> */}
       <div className="row mt-4">
         <div className="col-md-6 d-flex justify-content-start">
           <button
