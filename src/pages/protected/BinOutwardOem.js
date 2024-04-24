d71156eb84f0c4ba373697c3999aa0f30cf40f69
Mani-ofl-account <kmanikandan@whydigit.com>
2024-04-24 10:58:28 +0530

    bin outward oem UI design

--- src/pages/protected/BinOutwardOem.js
+++ src/pages/protected/BinOutwardOem.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setPageTitle } from "../../features/common/headerSlice";
-import { BinOutwardOem } from "../../features/emitter/BinOutwardOem";
+import BinOutwardOem from "../../features/emitter/BinOutwardOem";
 
 function InternalPage() {
   const dispatch = useDispatch();
